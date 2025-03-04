5bf976b4936426de15c5c36d7503820c26ad3da5
daffa-d <daffa123destrata@gmail.com>
2025-03-04 15:16:47 +0700

    Adding FFT Algorithm

--- src/main.cpp
+++ src/main.cpp
@@ -3,11 +3,24 @@
 #include "Adafruit_VL53L0X.h"
 #include "I2Cdev.h"
 #include "MPU6050_6Axis_MotionApps20.h"
+#include "arduinoFFT.h"
+#include "freertos/queue.h"
 
 #define OUTPUT_READABLE_YAWPITCHROLL
+#define EARTH_GRAVITY_MS2 9.80665
+
+#define SAMPLES 64
+#define SAMPLING_FREQUENCY 1000
 
 Adafruit_VL53L0X lox = Adafruit_VL53L0X();
 MPU6050 mpu;
+
+
+double vReal[SAMPLES];
+double vImag[SAMPLES];
+ArduinoFFT<double> FFT = ArduinoFFT<double>(vReal, vImag, SAMPLES, SAMPLING_FREQUENCY);
+
+xQueueHandle dataVibration_Queue = NULL;
 
 int const INTERRUPT_PIN = 2; // Define the interruption #0 pin
 bool blinkState;
@@ -20,56 +33,42 @@ uint8_t FIFOBuffer[64];
 
 Quaternion q;
 VectorInt16 aa;
-VectorInt16 gy;      
-VectorInt16 aaReal;  
-VectorInt16 aaWorld; 
-VectorFloat gravity; 
-float euler[3];      
-float ypr[3];        
+VectorInt16 gy;
+VectorInt16 aaWorld;
+VectorFloat gravity;
+volatile bool MPUInterrupt = false;
+float gFactor = 2000.0 / 32767.0;
 
-uint8_t teapotPacket[14] = {'$', 0x02, 0, 0, 0, 0, 0, 0, 0, 0, 0x00, 0x00, '\r', '\n'};
-
-volatile bool MPUInterrupt = false; 
 void DMPDataReady()
 {
   MPUInterrupt = true;
 }
+void readIMU(void *pvParameters);
+void FFT_Processing(void *pvParameters);
 
 void setup()
 {
   Wire.begin();
   Wire.setClock(400000);
-
-
-  Serial.begin(115200); 
-  while (!Serial)
-    ;
+  Serial.begin(115200);
 
   mpu.initialize();
+  Serial.println(mpu.getRate());
   pinMode(INTERRUPT_PIN, INPUT);
+  pinMode(LED_BUILTIN, OUTPUT);
 
   if (mpu.testConnection() == false)
   {
     Serial.println("MPU6050 connection failed");
-    while (true);
+    while (true)
+      ;
   }
   else
   {
     Serial.println("MPU6050 connection successful");
   }
 
-  Serial.println(F("\nSend any character to begin: "));
-  while (Serial.available() && Serial.read())
-    ; // Empty buffer
-  while (!Serial.available())
-    ; // Wait for data
-  while (Serial.available() && Serial.read())
-    ; // Empty buffer again
-
-  Serial.println(F("Initializing DMP..."));
   devStatus = mpu.dmpInitialize();
-
-  /* Supply your gyro offsets here, scaled for min sensitivity */
   mpu.setXGyroOffset(0);
   mpu.setYGyroOffset(0);
   mpu.setZGyroOffset(0);
@@ -77,16 +76,14 @@ void setup()
   mpu.setYAccelOffset(0);
   mpu.setZAccelOffset(0);
 
-  /* Making sure it worked (returns 0 if so) */
   if (devStatus == 0)
   {
-    mpu.CalibrateAccel(6); 
+    mpu.CalibrateAccel(6);
     mpu.CalibrateGyro(6);
     mpu.PrintActiveOffsets();
     mpu.setDMPEnabled(true);
     attachInterrupt(digitalPinToInterrupt(INTERRUPT_PIN), DMPDataReady, RISING);
     MPUIntStatus = mpu.getIntStatus();
-
     DMPReady = true;
     packetSize = mpu.dmpGetFIFOPacketSize();
   }
@@ -96,28 +93,59 @@ void setup()
     Serial.print(devStatus);
     Serial.println(F(")"));
   }
-  pinMode(LED_BUILTIN, OUTPUT);
+
+  dataVibration_Queue = xQueueCreate(10, sizeof(float));
+
+  xTaskCreatePinnedToCore(readIMU, "readIMU", 4096, NULL, 1, NULL, 0);
+  xTaskCreatePinnedToCore(FFT_Processing, "FFT_Processing", 4096, NULL, 1, NULL, 1);
 }
 
 void loop()
 {
-  if (!DMPReady)
-    return;
+}
 
-  if (mpu.dmpGetCurrentFIFOPacket(FIFOBuffer))
+void readIMU(void *pvParameters)
+{
+  for (;;)
   {
+    if (!DMPReady) return;
 
-    mpu.dmpGetQuaternion(&q, FIFOBuffer);
-    mpu.dmpGetGravity(&gravity, &q);
-    mpu.dmpGetYawPitchRoll(ypr, &q, &gravity);
+    
+    if (mpu.dmpGetCurrentFIFOPacket(FIFOBuffer)) {
+      mpu.dmpGetQuaternion(&q, FIFOBuffer);
+      mpu.dmpGetGravity(&gravity, &q);
+      mpu.dmpGetAccel(&aa, FIFOBuffer);
+      mpu.dmpConvertToWorldFrame(&aaWorld, &aa, &q);
 
-    mpu.dmpGetQuaternion(&q, FIFOBuffer);
-    mpu.dmpGetAccel(&aa, FIFOBuffer);
-    mpu.dmpGetGravity(&gravity, &q);
-    mpu.dmpGetLinearAccel(&aaReal, &aa, &gravity);
+      float X_Accel = aaWorld.x * mpu.get_acce_resolution() * EARTH_GRAVITY_MS2;
+      float Y_Accel = aaWorld.y * mpu.get_acce_resolution() * EARTH_GRAVITY_MS2;
+      float Z_Accel = aaWorld.z * mpu.get_acce_resolution() * EARTH_GRAVITY_MS2;
 
-    /* Blink LED to indicate activity */
-    blinkState = !blinkState;
-    digitalWrite(LED_BUILTIN, blinkState);
+      float vibration = sqrt(pow(X_Accel, 2) + pow(Y_Accel, 2) + pow(Z_Accel, 2));
+      xQueueSend(dataVibration_Queue, &vibration, 0);
+    }
+    vTaskDelay(10 / portTICK_PERIOD_MS);
+  }
+}
+
+void FFT_Processing(void *pvParamters){
+
+  float vibration;
+  for(;;) {
+    if(xQueueReceive(dataVibration_Queue, &vibration, portMAX_DELAY) == pdTRUE) {
+      for(int i = 0; i < SAMPLES; i++) {
+        vReal[i] = vibration;
+        vImag[i] = 0;
+        vTaskDelay(10 / portTICK_PERIOD_MS);
+      }
+    }
+
+    FFT.windowing(FFT_WIN_TYP_HAMMING, FFT_FORWARD);
+    FFT.compute(FFT_FORWARD);
+    FFT.complexToMagnitude();
+    
+    float peak = FFT.majorPeak();
+    Serial.println(peak, 6);
+    vTaskDelay(100 / portTICK_PERIOD_MS);
   }
 }
