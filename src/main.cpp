420f679918d11c1993a3dce47b1625022d7e35b0
daffa-d <daffa123destrata@gmail.com>
2025-03-30 14:10:44 +0700

    Refactor IMU reading and calibration process; adjust sample size and frequency

--- src/main.cpp
+++ src/main.cpp
@@ -5,11 +5,11 @@
 #include "MPU6050_6Axis_MotionApps20.h"
 #include "arduinoFFT.h"
 #include "freertos/queue.h"
+#include "esp_task_wdt.h"
 
 #define EARTH_GRAVITY_MS2 9.80665
-
-#define SAMPLES 4096
-#define SAMPLING_FREQUENCY 50
+#define SAMPLES 1024
+#define SAMPLING_FREQUENCY 1000
 
 Adafruit_VL53L0X lox = Adafruit_VL53L0X();
 MPU6050 mpu;
@@ -18,80 +18,45 @@ double vReal[SAMPLES];
 double vImag[SAMPLES];
 ArduinoFFT<double> FFT = ArduinoFFT<double>(vReal, vImag, SAMPLES, SAMPLING_FREQUENCY);
 
-int const INTERRUPT_PIN = 2; // Define the interruption #0 pin
-bool blinkState;
+int16_t ax, ay, az;
+unsigned long lastTime = 0;
 
-bool DMPReady = false;
-uint8_t MPUIntStatus;
-uint8_t devStatus;
-uint16_t packetSize;
-uint8_t FIFOBuffer[64];
-uint32_t nextSampleTime;
-
-Quaternion q;
-VectorInt16 aa;
-VectorInt16 gy;
-VectorInt16 aaWorld;
-VectorFloat gravity;
-VectorInt16 aaReal;
-volatile bool MPUInterrupt = false;
-
-void DMPDataReady()
-{
-  MPUInterrupt = true;
-}
 void readIMU(void *pvParameters);
 
 void setup()
 {
   Serial.begin(115200);
   Wire.begin();
-  Wire.setClock(400000);
 
-  mpu.initialize();
-  pinMode(INTERRUPT_PIN, INPUT);
   pinMode(LED_BUILTIN, OUTPUT);
 
+  mpu.initialize();
   if (mpu.testConnection() == false)
   {
     Serial.println("MPU6050 connection failed");
-    while (true)
-      ;
-  }
-  else
-  {
-    Serial.println("MPU6050 connection successful");
+    int i = 0;
+    while (i < 5)
+    {
+      digitalWrite(LED_BUILTIN, HIGH);
+      vTaskDelay(2000 / portTICK_PERIOD_MS);
+      digitalWrite(LED_BUILTIN, LOW);
+      vTaskDelay(2000 / portTICK_PERIOD_MS);
+      i++;
+    }
+    if (i == 5)
+      ESP.restart();
   }
 
-  devStatus = mpu.dmpInitialize();
-  mpu.setXGyroOffset(0);
-  mpu.setYGyroOffset(0);
-  mpu.setZGyroOffset(0);
-  mpu.setXAccelOffset(0);
-  mpu.setYAccelOffset(0);
-  mpu.setZAccelOffset(0);
+  // mpu.setXAccelOffset(0);
+  // mpu.setYAccelOffset(0);
+  // mpu.setZAccelOffset(0);
+  // mpu.setXGyroOffset(0);
+  // mpu.setYGyroOffset(0);
+  // mpu.setZGyroOffset(0);
 
-  if (devStatus == 0)
-  {
-    mpu.CalibrateAccel(6);
-    mpu.CalibrateGyro(6);
-    // mpu.PrintActiveOffsets();
-    mpu.setDMPEnabled(true);
-    attachInterrupt(digitalPinToInterrupt(INTERRUPT_PIN), DMPDataReady, RISING);
-    MPUIntStatus = mpu.getIntStatus();
-    DMPReady = true;
-    packetSize = mpu.dmpGetFIFOPacketSize();
-  }
-  else
-  {
-    Serial.print(F("DMP Initialization failed (code "));
-    Serial.print(devStatus);
-    Serial.println(F(")"));
-  }
+  mpu.CalibrateAccel(6);
 
   xTaskCreatePinnedToCore(readIMU, "readIMU", 4096, NULL, 1, NULL, 0);
-
-  nextSampleTime = micros();
 }
 
 void loop()
@@ -100,31 +65,48 @@ void loop()
 
 void readIMU(void *pvParameters)
 {
-  float vibration = 0;
+  int retry = 0;
   for (;;)
   {
-    if (!DMPReady)
-      continue;
+    lastTime = micros();
 
     for (int i = 0; i < SAMPLES; i++)
     {
+      mpu.getAcceleration(&ax, &ay, &az);
 
-      if (mpu.dmpGetCurrentFIFOPacket(FIFOBuffer))
+      float accelX_g = ax / 16384.0;
+      float accelY_g = ay / 16384.0;
+      float accelZ_g = az / 16384.0;
+
+      float accelX_ms2 = accelX_g * 9.80665;
+      float accelY_ms2 = accelY_g * 9.80665;
+      float accelZ_ms2 = accelZ_g * 9.80665;
+
+      if (isnan(accelX_ms2) || isnan(accelY_ms2) || isnan(accelZ_ms2))
       {
-        mpu.dmpGetQuaternion(&q, FIFOBuffer);
-        mpu.dmpGetAccel(&aa, FIFOBuffer);
-        mpu.dmpGetGravity(&gravity, &q);
-        mpu.dmpGetLinearAccel(&aaReal, &aa, &gravity);
-        
-        float X_Accel = aaReal.x * mpu.get_acce_resolution();
-        float Y_Accel = aaReal.y * mpu.get_acce_resolution();
-        
-        vibration = sqrt(X_Accel * X_Accel + Y_Accel * Y_Accel);
-        vReal[i] = vibration;
-        vImag[i] = 0;
-      } else {
-        i--;
+        retry++;
+        vTaskDelay(10 / portTICK_PERIOD_MS);
       }
+      else if (accelX_ms2 == 0 || accelY_ms2 == 0 || accelZ_ms2 == 0)
+      {
+        retry++;
+        vTaskDelay(10 / portTICK_PERIOD_MS);
+      }
+      else
+      {
+        retry = 0;
+      }
+
+      if (retry > 5)
+      {
+        ESP.restart();
+      }
+
+      vReal[i] = accelX_ms2;
+      vImag[i] = 0;
+
+      while (micros() - lastTime < (1000000 / SAMPLING_FREQUENCY));
+      lastTime = micros();
     }
 
     FFT.dcRemoval();
@@ -132,11 +114,12 @@ void readIMU(void *pvParameters)
     FFT.compute(FFT_FORWARD);
     FFT.complexToMagnitude();
 
-    for (int i = 1; i < (SAMPLES / 2); i++)
-    {
-      double freq = (i * SAMPLING_FREQUENCY) / SAMPLES;
-      Serial.printf("%f\n", vReal[i]);
-    }
+    for (int i = 1; i < SAMPLES / 2; i++) {
+      double frequency = (i * SAMPLING_FREQUENCY) / SAMPLES;
+      Serial.print(frequency);
+      Serial.print(",");
+      Serial.println(vReal[i]);
+  }
 
     vTaskDelay(10 / portTICK_PERIOD_MS);
   }
